820ef24da8493548e3da66ca249ff03491a96dde
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2022-07-01 01:33:42 +0300

    post from json file

--- api_test/create_user2.py
+++ api_test/create_user2.py
@@ -7,4 +7,5 @@ my_data = open("data.json", "r").read()
 resp = requests.post("https://reqres.in/api/users", data=json.loads(my_data))
 print(resp)
 print(resp.json())
+print(resp.headers.get("Content-Type"))
 assert resp.json()["job"] == "python dev"
